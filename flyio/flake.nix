48d66f76b52b99d1277f0384b50a1f13592b4305
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2023-08-14 19:05:41 -0300

    feat: add flyio

---
+++ flyio/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Nix-flake-based Fly.io development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [
+        (self: super: rec {
+          flyctl = super.flyctl;
+        })
+      ];
+      pkgs = import nixpkgs { inherit overlays system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ flyctl ];
+
+        shellHook = ''
+        '';
+      };
+    });
+}
