aa80f581f9c53f2e6f047907b3395f776000bcce
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2023-11-23 17:44:10 -0300

    feat: add flutter

--- flutter/flake.nix
+++ flutter/flake.nix
@@ -3,17 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    devshell.url = "github:numtide/devshell";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
     android-nixpkgs.url = "github:tadfisher/android-nixpkgs/stable";
     android-nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
-    android-nixpkgs.inputs.devshell.follows = "devshell";
   };
 
-  outputs = { self, android-nixpkgs }: {
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , android-nixpkgs
+    }: {
     packages.x86_64-darwin.android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
       cmdline-tools-latest
       build-tools-34-0-0
