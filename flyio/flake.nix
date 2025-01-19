0dff8b1577429f4595e5d865bd55f76478ad46be
Guilherme Mangabeira Gregio <guilherme@gregio.net>
2025-01-19 18:16:00 -0300

    feat: update flyio

--- flyio/flake.nix
+++ flyio/flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-flake-based Fly.io development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
